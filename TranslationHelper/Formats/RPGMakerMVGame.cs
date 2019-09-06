9171c947d91c8d498a8cbc40c84ca758b45fda40
TokcDK <tokcdk@gmail.com>
2019-09-06 21:28:02 +0500

    formats experiments some code rearrange

---
+++ TranslationHelper/Formats/RPGMakerMVGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslationHelper.Formats
+{
+    internal class RPGMakerMVGame : IOpenFormats, ISaveFormats
+    {
+        public bool Open()
+        {
+            return false;
+        }
+
+        public bool Save()
+        {
+            return false;
+        }
+    }
+}
---
+++ TranslationHelper/Formats/RPGMakerTransPatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslationHelper.Formats
+{
+    internal class RPGMakerTransPatch : IOpenFormats, ISaveFormats
+    {
+        public bool Open()
+        {
+            return false;
+        }
+
+        public bool Save()
+        {
+            return false;
+        }
+    }
+}
