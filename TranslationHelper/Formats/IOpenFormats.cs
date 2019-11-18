21591914c3b09d13147f3bbf661c88bac680ace1
TokcDK <tokcdk@gmail.com>
2019-11-18 14:11:09 +0500

    some actions in path to convert code to strategy pattern

--- TranslationHelper/Formats/IOpenFormats.cs
+++ TranslationHelper/Formats/IOpenFormats.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-//http://qaru.site/questions/634338/design-pattern-parsing-similar-but-differing-schemas-in-text-files
+﻿//http://qaru.site/questions/634338/design-pattern-parsing-similar-but-differing-schemas-in-text-files
 namespace TranslationHelper.Formats
 {
     public interface IOpenFormats
---
+++ TranslationHelper/Projects/ProjectBase.cs
@@ -0,0 +1,21 @@
+﻿using TranslationHelper.Formats;
+
+namespace TranslationHelper.Projects
+{
+    public abstract class ProjectBase
+    {
+        protected IOpenFormats openFormat;
+        protected ISaveFormats saveFormat;
+
+        protected ProjectBase()
+        {
+            openFormat = new RPGMakerTransPatch();
+            saveFormat = new RPGMakerTransPatch();
+        }
+
+        public static string GetProjectName()
+        {
+            return "RPGMakerTransPatch";
+        }
+    }
+}
