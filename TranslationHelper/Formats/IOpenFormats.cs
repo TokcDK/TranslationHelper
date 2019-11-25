6c9865cdb9dd09e29baf5e5702328c03f403f7d7
TokcDK <tokcdk@gmail.com>
2019-11-25 11:32:21 +0500

    removed old files

--- TranslationHelper/Formats/IOpenFormats.cs
+++
@@ -1,8 +0,0 @@
-﻿//http://qaru.site/questions/634338/design-pattern-parsing-similar-but-differing-schemas-in-text-files
-namespace TranslationHelper.Formats
-{
-    public interface IOpenFormats
-    {
-        bool Open(string sPath);
-    }
-}
