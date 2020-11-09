45fa5e1ac145e1662cf7d66077096a73a20b9c29
TokcDK <tokcdk@gmail.com>
2020-11-09 20:13:06 +0500

    trying to remade online translation function in row function

---
+++ TranslationHelper/Functions/FileElementsFunctions/Row/OnlineTranslate.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using TranslationHelper.Data;
+using TranslationHelper.Extensions;
+
+namespace TranslationHelper.Functions.FileElementsFunctions.Row
+{
+    class OnlineTranslate : FileElementsRowFunctionsBase
+    {
+        /// <summary>
+        /// buffer for lines
+        /// </summary>
+        internal class Buffer
+        {
+            internal string GetOriginal { get; }
+            internal string GetTranslation { get; set; }
+            internal bool GetIsExtracted { get; }
+
+            internal Buffer(string Original, string Translation, bool IsExtracted)
+            {
+                GetOriginal = Original;
+                GetTranslation = Translation;
+                GetIsExtracted = IsExtracted;
+            }
+        }
+
+        Dictionary<int[], Dictionary<string, string>> buffer;
+
+        int Size { get; set; }
+        static int MaxSize { get => 1000; }
+
+        bool IsMax()
+        {
+            return Size >= MaxSize;
+        }
+
+        public OnlineTranslate(THDataWork thDataWork) : base(thDataWork)
+        {
+            if (buffer == null)
+            {
+                buffer = new Dictionary<int[], Dictionary<string, string>>();
+            }
+            if (Translator == null)
+            {
+                Translator = new GoogleAPIOLD(thDataWork);
+            }
+        }
+
+        protected override bool Apply()
+        {
+            if (SelectedRow[1] == null || (SelectedRow[1] as string).Length == 0)
+            {
+                int lineNum = 0;
+                foreach (var line in (SelectedRow[0] as string).SplitToLines())
+                {
+                    var lineCoordinates = new int[2] { SelectedTableIndex, SelectedRowIndex };
+
+                    //add lineCoordinates and row data
+                    if (!buffer.ContainsKey(lineCoordinates))
+                        buffer.Add(lineCoordinates, new Dictionary<string, string>());
+
+                    buffer[lineCoordinates].Add(line,null);
+
+
+
+                    Size += line.Length;
+
+                    if (IsMax())
+                    {
+                        TranslateAddedStrings();
+                    }
+
+                    lineNum++;
+                }
+            }
+
+            return false;
+        }
+
+        readonly GoogleAPIOLD Translator;
+        private void TranslateAddedStrings()
+        {
+            var originals = GetOriginals();
+
+            string[] translated = null;
+            try
+            {
+                var OriginalLinesPreapplied = ApplyProjectPretranslationAction(originals);
+                translated = Translator.Translate(OriginalLinesPreapplied);
+                if (translated == null || originals.Length != translated.Length)
+                {
+                    return;
+                }
+                translated = ApplyProjectPosttranslationAction(originals, translated);
+            }
+            catch (Exception ex)
+            {
+                new FunctionsLogs().LogToFile("Error while translation:"
+                    + Environment.NewLine
+                    + ex
+                    + Environment.NewLine
+                    + "OriginalLines="
+                    + string.Join(Environment.NewLine + "</br>" + Environment.NewLine, originals));
+            }
+        }
+
+        private string[] GetOriginals()
+        {
+            List<string> orig = new List<string>();
+            foreach (var line in buffer)
+            {
+            }
+
+            return orig.ToArray();
+        }
+
+        private string[] ApplyProjectPretranslationAction(string[] originalLines)
+        {
+            if (thDataWork.CurrentProject.HideVARSMatchCollectionsList != null && thDataWork.CurrentProject.HideVARSMatchCollectionsList.Count > 0)
+            {
+                thDataWork.CurrentProject.HideVARSMatchCollectionsList.Clear();//clean of found maches collections
+            }
+
+            for (int i = 0; i < originalLines.Length; i++)
+            {
+                var s = thDataWork.CurrentProject.OnlineTranslationProjectSpecificPretranslationAction(originalLines[i], string.Empty);
+                if (!string.IsNullOrEmpty(s))
+                {
+                    originalLines[i] = s;
+                }
+            }
+            return originalLines;
+        }
+
+        private string[] ApplyProjectPosttranslationAction(string[] originalLines, string[] translatedLines)
+        {
+            for (int i = 0; i < translatedLines.Length; i++)
+            {
+                var s = thDataWork.CurrentProject.OnlineTranslationProjectSpecificPosttranslationAction(originalLines[i], translatedLines[i]);
+                if (!string.IsNullOrEmpty(s) && s != translatedLines[i])
+                {
+                    translatedLines[i] = s;
+                }
+            }
+
+            if (thDataWork.CurrentProject.HideVARSMatchCollectionsList != null && thDataWork.CurrentProject.HideVARSMatchCollectionsList.Count > 0)
+            {
+                thDataWork.CurrentProject.HideVARSMatchCollectionsList.Clear();//clean of found maches collections
+            }
+
+            return translatedLines;
+        }
+    }
+}
