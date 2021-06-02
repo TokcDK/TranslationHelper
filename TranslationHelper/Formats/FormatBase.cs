ea7bc41968a7a9f948180f84e5f863d6c33dfa3d
Denis K <tokcdk@gmail.com>
2021-06-02 18:41:13 +0500

    some comment to added extidentifier

--- TranslationHelper/Formats/FormatBase.cs
+++ TranslationHelper/Formats/FormatBase.cs
@@ -39,7 +39,8 @@ namespace TranslationHelper.Formats
         }
 
         /// <summary>
-        /// identifier to check how to identifi if selected extension must be parsed with this format
+        /// identifier to check how to identifi if selected extension must be parsed with this format.
+        /// in result can be added new project which will be used Ext and this identifier to open valid standalone files.
         /// </summary>
         /// <returns></returns>
         internal virtual bool ExtIdentifier()
