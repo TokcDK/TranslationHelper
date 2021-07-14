7a44ea7d8e07ba7a0a330428647ef34b32357540
Denis K <tokcdk@gmail.com>
2021-07-14 14:16:50 +0500

    added ngettext

--- THCore/SharedData.cs
+++ THCore/SharedData.cs
@@ -1,12 +1,16 @@
-﻿namespace THCore
+﻿using l10n;
+
+namespace THCore
 {
     public static class SharedData
     {
+        internal static string LanguageDirName { get => "l10n"; }
+
         public static string StartupPath { get; set; }
         public static string MenuModulesDirPath { get; set; }
         public static class Menus
         {
-            public static string FileMenuName = "File";
+            public static string FileMenuName = T._("File");
         }
     }
 }
---
+++ THCore/T.cs
@@ -0,0 +1,49 @@
+﻿using NGettext;
+using System.IO;
+using THCore;
+
+//
+// Usage:
+//		T._("Hello, World!"); // GetString
+//		T._n("You have {0} apple.", "You have {0} apples.", count, count); // GetPluralString
+//		T._p("Context", "Hello, World!"); // GetParticularString
+//		T._pn("Context", "You have {0} apple.", "You have {0} apples.", count, count); // GetParticularPluralString
+//
+//  https://habr.com/ru/post/432786/
+//  https://github.com/VitaliiTsilnyk/NGettext
+namespace l10n
+{
+    internal class T
+    {
+        static Catalog GetGlobal()
+        {
+            var localesDir = Path.Combine(Directory.GetCurrentDirectory(), SharedData.LanguageDirName);
+            //_Catalog = new Catalog("en", localesDir, new CultureInfo("en-EN"));
+            //_Catalog = new Catalog("ru", localesDir, new CultureInfo("ru-RU"));
+            //_Catalog = new Catalog("th", localesDir);
+            return new Catalog("ln", localesDir);
+        }
+
+        //private static readonly ICatalog _Catalog = new Catalog("Example", "./locale");
+        private static readonly ICatalog _Catalog = GetGlobal();
+
+        public static string _(string text) => _Catalog.GetString(text);
+
+        public static string _(string text, params object[] args) => _Catalog.GetString(text, args);
+
+#pragma warning disable IDE1006 // Стили именования
+        public static string _n(string text, string pluralText, long n) => _Catalog.GetPluralString(text, pluralText, n);
+
+
+        public static string _n(string text, string pluralText, long n, params object[] args) => _Catalog.GetPluralString(text, pluralText, n, args);
+
+        public static string _p(string context, string text) => _Catalog.GetParticularString(context, text);
+
+        public static string _p(string context, string text, params object[] args) => _Catalog.GetParticularString(context, text, args);
+
+        public static string _pn(string context, string text, string pluralText, long n) => _Catalog.GetParticularPluralString(context, text, pluralText, n);
+
+        public static string _pn(string context, string text, string pluralText, long n, params object[] args) => _Catalog.GetParticularPluralString(context, text, pluralText, n, args);
+#pragma warning restore IDE1006 // Стили именования
+    }
+}
