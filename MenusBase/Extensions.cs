415ad38229805e858e98ea4cad076e9b9c0f8be1
Denis K <tokcdk@gmail.com>
2021-07-14 13:15:51 +0500

    description for custom contains menu's extension method

--- MenusBase/Extensions.cs
+++ MenusBase/Extensions.cs
@@ -20,6 +20,13 @@ namespace Menus
             return null;
         }
 
+        /// <summary>
+        /// check if toolStripMenuItem contains item with itemWithText
+        /// </summary>
+        /// <param name="toolStripMenuItem">Source menu where to search item with text</param>
+        /// <param name="itemWithText">menu's text which to search</param>
+        /// <param name="foundFoolStripMenuItem">set found ToolStripMenuItem to this else null</param>
+        /// <returns>true if found and set out item, else false and null</returns>
         public static bool Contains(this MenuStrip toolStripMenuItem, string itemWithText, out ToolStripMenuItem foundFoolStripMenuItem)
         {
             foreach (ToolStripMenuItem item in toolStripMenuItem.Items)
