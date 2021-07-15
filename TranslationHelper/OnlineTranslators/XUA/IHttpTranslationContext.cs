0af6f8c3e8cd2ec8fcd2b10c29e2c9ea49b8c155
Denis K <tokcdk@gmail.com>
2021-07-15 12:43:05 +0500

    removed some external files

--- TranslationHelper/OnlineTranslators/XUA/IHttpTranslationContext.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TranslationHelper.OnlineTranslators.XUA
-{
-    /// <summary>
-    /// Interface used in the context of the HttpEndpoint.
-    /// </summary>
-    public interface IHttpTranslationContext : ITranslationContextBase
-    {
-    }
-}
