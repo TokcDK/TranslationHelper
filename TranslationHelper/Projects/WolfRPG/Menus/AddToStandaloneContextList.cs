1e0452e66f0a11e78fb79e039ffca14bb4a1de1c
Denis K <tokcdk@gmail.com>
2021-11-08 22:11:26 +0500

    added add to standalone context list menu class to add in some menu and use for projects using rpgmwolf patch to separate some strings using context from the list

---
+++ TranslationHelper/Projects/WolfRPG/Menus/AddToStandaloneContextList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using TranslationHelper.Data;
+using TranslationHelper.Menus.ProjectMenus;
+
+namespace TranslationHelper.Projects.WolfRPG.Menus
+{
+    class AddToStandaloneContextList : IMenu
+    {
+        public string Text => "Add standalone context";
+
+        public string Description => "Adds entered context as standalone. Each string using the context path will be moved in separated text block in patch in time of open/save";
+
+        public string Category => "";
+
+        public void OnClick(object sender, EventArgs e)
+        {
+            var standaloneContextFilePath = Path.Combine(ProjectData.SelectedDir, "StandaloneContextList.thdata");
+            var standaloneContextList = new List<string>();
+            if (File.Exists(standaloneContextFilePath))
+            {
+                using(StreamReader reader = new StreamReader(standaloneContextFilePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        if (string.IsNullOrWhiteSpace(line))
+                        {
+                            continue;
+                        }
+
+                        standaloneContextList.Add(line);
+                    }
+                }
+            }
+
+            var form = new AddToStandaloneContextListForm();
+
+            var result = form.ShowDialog();
+            if(result != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
+
+            var addedContextLine = form.ContextLine;
+
+            standaloneContextList.Add(addedContextLine);
+
+            File.WriteAllLines(standaloneContextFilePath, standaloneContextList);
+        }
+    }
+}
---
+++ TranslationHelper/Projects/WolfRPG/Menus/AddToStandaloneContextListForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TranslationHelper.Projects.WolfRPG.Menus
+{
+    public partial class AddToStandaloneContextListForm : Form
+    {
+        public AddToStandaloneContextListForm()
+        {
+            InitializeComponent();
+        }
+
+        public string ContextLine = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
