44959428091f9d645e45ceac834e6694615ec735
Denis K <tokcdk@gmail.com>
2022-03-22 18:39:00 +0500

    added po file open

---
+++ TranslationHelper/Formats/Other/PO.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TranslationHelper.Formats.Other
+{
+    internal class PO : FormatStringBase
+    {
+        internal override string Ext()
+        {
+            return ".po";
+        }
+
+        bool FindOriginal = true;
+        string original;
+        protected override KeywordActionAfter ParseStringFileLine()
+        {
+            if (FindOriginal)
+            {
+                if (ParseData.Line.StartsWith("msgid"))
+                {
+                    original = Regex.Match(ParseData.Line, @"^msgid \""(.*)\""$").Groups[1].Value;
+                    if (!string.IsNullOrEmpty(original))
+                    {
+                        FindOriginal = false;
+                    }
+                }
+            }
+            else
+            {
+                if (ParseData.Line.StartsWith("msgstr"))
+                {
+                    FindOriginal = true;
+                    AddRowData(ref original, Regex.Match(ParseData.Line, @"^msgstr \""(.*)\""$").Groups[1].Value, false);
+                    ParseData.Ret = true;
+                }
+            }
+
+            return KeywordActionAfter.Continue;
+        }
+    }
+}
