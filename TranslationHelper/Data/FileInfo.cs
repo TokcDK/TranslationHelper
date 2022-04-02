01d0b83bc8c4568bbb86be6aefb9080bc59639a3
Denis K <tokcdk@gmail.com>
2022-04-02 19:55:13 +0500

    updated file info format

--- TranslationHelper/Data/FileInfo.cs
+++ TranslationHelper/Data/FileInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System.Collections.Generic;
+using System.Data;
 using System.IO;
 
 namespace TranslationHelper.Data
@@ -9,6 +10,29 @@ namespace TranslationHelper.Data
 
         public string Info = "";
 
-        public DataTable Content;
+        public FileContent Content;
+    }
+
+    public class FileContent : List<FileRow> { /* list of FileRow here*/ }
+
+    public class FileRow
+    {
+        public DataRow Row;
+        public string Info = "";
+    }
+
+    public static class TestData
+    {
+        public static ProjectFileInfo GetFile()
+        {
+            return new ProjectFileInfo()
+            {
+                File = new FileInfo(@"c:\file.txt"),
+                Content = new FileContent()
+            {
+                new FileRow(){ Row = null}
+            }
+            };
+        }
     }
 }
