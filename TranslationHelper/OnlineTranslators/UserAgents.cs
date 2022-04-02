2dd0cadb79eb55aa3b91a2f7f0bac6e2b4f4f62c
Denis K <tokcdk@gmail.com>
2022-04-02 10:22:19 +0500

    updated useragent

--- TranslationHelper/OnlineTranslators/UserAgents.cs
+++ TranslationHelper/OnlineTranslators/UserAgents.cs
@@ -5,6 +5,6 @@
         /// <summary>
         /// Chrome useragent
         /// </summary>
-        public static readonly string Chrome_Win7 = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4700.1 Iron Safari/537.36";
+        public static readonly string Chrome_Win7 = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.5000.0 Iron Safari/537.36";
     }
 }
