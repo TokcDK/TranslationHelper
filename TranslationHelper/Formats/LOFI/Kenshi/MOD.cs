f1335c7d4882e7c8b044f80597c9a427019ec16f
Denis K <tokcdk@gmail.com>
2022-04-10 22:28:32 +0500

    tried to add kenshi mod file open

--- TranslationHelper/Formats/LOFI/Kenshi/MOD.cs
+++ TranslationHelper/Formats/LOFI/Kenshi/MOD.cs
@@ -1,114 +1,32 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Text;
+﻿//using KenshModTIO;
 using TranslationHelper.Data;
-using TranslationHelper.Extensions;
-using OpenConstructionSet.Data;
-using OpenConstructionSet.Installations;
-using OpenConstructionSet.Mods;
-using OpenConstructionSet.Mods.Context;
-using System.Linq;
-using System;
-using System.Collections.Specialized;
 
 namespace TranslationHelper.Formats.LOFI.Kenshi
 {
-    abstract class MOD : FormatBinaryBase
+    class MOD : FormatStringBase
     {
-        public MOD()
+        internal override bool Open()
         {
-        }
+            AddTables();
 
-        internal override string Ext()
-        {
-            return ".mod";
-        }
+            //var reader = new KenshModIO();
 
-        protected override void ParseBytes()
-        {
-            Parse();
-
-            base.ParseBytes();
-        }
-
-        private async void Parse()
-        {
-            //ModFile referenceMod = new ModFile(ProjectData.SelectedFilePath);
-
-            //ModFileData referenceData;
-
-            //try
+            //foreach (var str in reader.GetStrings(ProjectData.SelectedGameDir, FilePath))
             //{
-            //    referenceData = await referenceMod.ReadDataAsync();
-            //}
-            //catch (Exception ex)
-            //{
-            //    return;
-            //    //Error($"Unable to load {ReferenceModName}{Environment.NewLine}Error: {ex}");
-            //    //return (0, 0, 0);
+            //    AddRowData(str, CheckInput: false);
             //}
 
-            //var items = new List<string>();
-            //foreach (var item in referenceData.Items)
-            //{
-            //    items.Add(item.Name);
-
-            //    foreach (var n in item.Values)
-            //    {
-            //        if (!(n.Value is string s)) continue;
-            //        if (n.Key != "description" && !n.Key.StartsWith("text")) continue;
-            //        if (string.IsNullOrWhiteSpace(s)) continue;
-
-            //        items.Add(s);
-            //    }
-            //}
-
-            //var iii = items;
+            return CheckTablesContent(FilePath);
         }
 
-        ///// <summary>
-        ///// start position in exe from where to start parse.
-        ///// Set it to offset of first byte of string.
-        ///// </summary>
-        //protected abstract long StartPos { get; }
-        ///// <summary>
-        ///// end position of the exe where to stop parse.
-        ///// set it to first not zero and not ff byte offset where scan for strings must stop
-        ///// </summary>
-        //protected abstract long EndPos { get; }
+        void OpenTHeFile()
+        {
+            //var reader = new KenshModIO();
 
-        //bool readstring;
-        //protected override bool FilePreOpenActions()
-        //{
-        //    var pre = new byte[] { 0x74, 0x65, 0x78, 0x74, 0x30 };
-        //    var post = new byte[] { 0x6C, 0x69, 0x6E, 0x65, 0x73 };
-        //    TextPrePostBytes.Add();
-        //    return base.FilePreOpenActions();
-        //}
-
-        //protected override void PreParseBytes()
-        //{
-        //    readstring = true;
-        //}
-
-        //protected override bool ParseBytesReadCondition()
-        //{
-        //    return ParseData.FStream.Position <= EndPos;
-        //}
-
-        //List<byte> searchbytes = new List<byte>();
-        //Dictionary<byte[], byte[]> TextPrePostBytes = new Dictionary<byte[], byte[]>();
-
-        //protected override KeywordActionAfter ParseByte()
-        //{
-        //    if (readstring)
-        //    {
-        //    }
-        //    else
-        //    {
-        //    }
-
-        //    return KeywordActionAfter.Continue;
-        //}
+            //foreach (var str in reader.GetStrings(ProjectData.SelectedGameDir, FilePath))
+            //{
+            //    AddRowData(str, CheckInput: false);
+            //}
+        }
     }
 }
---
+++ TranslationHelper/Projects/LO-FI/KenshiMod.cs
@@ -0,0 +1,74 @@
+﻿using KenshModTIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TranslationHelper.Data;
+using TranslationHelper.Formats.LOFI.Kenshi;
+using TranslationHelper.Projects.ZZZZFormats;
+
+namespace TranslationHelper.Projects.LO_FI
+{
+    internal class KenshiMod : ProjectBase
+    {
+        internal override bool Check()
+        {
+            bool b1 = IsExe();
+            bool b2 = Path.GetFileName(ProjectData.SelectedFilePath).StartsWith("kenshi");
+            bool b3 = Directory.Exists(Path.Combine(Path.GetDirectoryName(ProjectData.SelectedFilePath), "mods"));
+            return b1 && b2 && b3;
+        }
+
+        internal override string Name()
+        {
+            return "Kenshi mod";
+        }
+
+        internal override bool Open()
+        {
+            var gamePath = Path.GetDirectoryName(ProjectData.SelectedFilePath);
+            List<string> foundTypes = new List<string>();
+            foreach(var dir in Directory.GetDirectories(Path.Combine(gamePath, "mods")))
+            {
+                var modFilePath = Path.Combine(dir, Path.GetFileNameWithoutExtension(dir)+".mod");
+
+                if (File.Exists(modFilePath)) foundTypes.Add(Path.GetFileNameWithoutExtension(modFilePath));
+            }
+
+            int selectedIndex = -1;
+            if (foundTypes.Count == 0) return false;
+
+            var foundForm = new FoundTypesbyExtensionForm();
+
+            foreach (var type in foundTypes)
+            {
+                foundForm.listBox1.Items.Add(type);
+            }
+
+            var result = foundForm.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                selectedIndex = foundForm.SelectedTypeIndex;
+            }
+
+            foundForm.Dispose();
+
+            if (selectedIndex == -1) return false;
+
+            var modName = foundTypes[selectedIndex];
+
+            var format = new MOD();
+            format.FilePath = modName;
+
+            return format.Open();
+        }
+
+        internal override bool Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
