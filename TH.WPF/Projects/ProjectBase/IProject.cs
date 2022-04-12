28010065881f041f0aec4e570237b6b971f1a52b
Denis K <tokcdk@gmail.com>
2022-04-12 09:55:36 +0500

    wpf:added project base to be using as project base dll

---
+++ TH.WPF/Projects/ProjectBase/IProject.cs
@@ -0,0 +1,28 @@
+﻿namespace ProjectBase
+{
+    interface IProject
+    {
+        /// <summary>
+        /// Title of the project
+        /// </summary>
+        string Title { get; }
+
+        /// <summary>
+        /// Conditions to determine if the project can open selected file or folder
+        /// </summary>
+        /// <returns></returns>
+        bool IsValid();
+
+        /// <summary>
+        /// Open actions for the project
+        /// </summary>
+        /// <returns></returns>
+        bool Open();
+
+        /// <summary>
+        /// Save actions for the project
+        /// </summary>
+        /// <returns></returns>
+        bool Save();
+    }
+}
