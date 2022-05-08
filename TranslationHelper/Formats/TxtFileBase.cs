47f009a2d8614b9003e561b90f2ea631ae4be4cf
Denis K <tokcdk@gmail.com>
2022-05-08 17:41:14 +0500

    added txt format with string per line parse

---
+++ TranslationHelper/Formats/TxtFileBase.cs
@@ -0,0 +1,10 @@
+﻿namespace TranslationHelper.Formats
+{
+    internal abstract class FormatTxtFileBase : FormatStringBase
+    {
+        internal override string Ext()
+        {
+            return ".txt";
+        }
+    }
+}
---
+++ TranslationHelper/Formats/zzzOtherFormat/TXTStringPerLine.cs
@@ -0,0 +1,15 @@
+﻿namespace TranslationHelper.Formats.zzzOtherFormat
+{
+    internal class TXTStringPerLine : FormatTxtFileBase
+    {
+        protected override KeywordActionAfter ParseStringFileLine()
+        {
+            AddRowData(ref ParseData.Line);
+
+            SaveModeAddLine();
+
+            return KeywordActionAfter.Continue;
+        }
+
+    }
+}
