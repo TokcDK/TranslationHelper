e7e852c6c0918b266b9f98a547c12f2d39da17ea
Denis K <tokcdk@gmail.com>
2022-05-20 19:06:29 +0500

    added wolfrpg evtxt open

---
+++ TranslationHelper/Formats/WolfRPG/EvTextTXT.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using TranslationHelper.Data;
+
+namespace TranslationHelper.Formats.WolfRPG
+{
+    internal class EvTextTXT : FormatTxtFileBase
+    {
+        readonly List<string> Buffer = new List<string>();
+        protected override KeywordActionAfter ParseStringFileLine()
+        {
+            if (ParseData.Line.Length > 0 && ParseData.Line[0]!='/')
+            {
+                Buffer.Add(ParseData.Line);
+
+                while (!string.IsNullOrEmpty(ReadLine()) && ParseData.Line[0] != '/')
+                {
+                    Buffer.Add(ParseData.Line);
+                }
+
+                var value = string.Join(Environment.NewLine, Buffer);
+                AddRowData(ref value);
+
+                if (ProjectData.SaveFileMode)
+                {
+                    ParseData.Line = value + Environment.NewLine + ParseData.Line;
+                }
+
+                Buffer.Clear();
+            }
+
+            SaveModeAddLine();
+
+            return KeywordActionAfter.Continue;
+        }
+    }
+}
