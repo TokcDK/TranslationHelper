b87097900338d22ea9bbf76b931ee9efb72420c4
Denis K <tokcdk@gmail.com>
2022-06-08 11:29:56 +0500

    rpgmv added sidplayname parse for map extra infos for events

--- TranslationHelper/Formats/RPGMMV/JsonType/JsonMap.cs
+++ TranslationHelper/Formats/RPGMMV/JsonType/JsonMap.cs
@@ -9,9 +9,15 @@ namespace TranslationHelper.Formats.RPGMMV.JsonType
         protected override object ParseJson(string path)
         {
             if (string.Equals(Path.GetFileNameWithoutExtension(path), "MapInfos", System.StringComparison.InvariantCultureIgnoreCase)) return null;
-            
+
             var data = Helper.LoadMap(path);
             if (data == null) return null;
+
+            var dsplayName = data.DisplayName;
+            if (AddRowData(ref dsplayName, $"Map Note: {data.Note}") && AppData.SaveFileMode)
+            {
+                data.DisplayName = dsplayName;
+            }
 
             int eventsCount = data.Events.Length;
             for (int e = 0; e < eventsCount; e++)
@@ -23,7 +29,7 @@ namespace TranslationHelper.Formats.RPGMMV.JsonType
                 for (int p = 0; p < pagesCount; p++)
                 {
                     var page = @event.Pages[p];
-                    ParseCommandStrings(page.Commands, $"Event ID: {@event.Id}\r\nEvent name: {@event.Name}\r\nPage number: {p}");
+                    ParseCommandStrings(page.Commands, AppData.SaveFileMode ? "" : $"Map DisplayName: \"{data.DisplayName}\"\r\nMap Note: \"{data.Note}\"\r\nEvent ID: {@event.Id}\r\nEvent name: \"{@event.Name}\"\r\nPage number: {p}");
                 }
             }
 
