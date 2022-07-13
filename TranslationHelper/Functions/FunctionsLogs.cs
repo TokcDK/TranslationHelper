6df00edeb0411fb65b98a23878b9e23a895cd71a
Denis K <tokcdk@gmail.com>
2022-07-13 20:31:33 +0500

    little reduced nesting

--- TranslationHelper/Functions/FunctionsLogs.cs
+++ TranslationHelper/Functions/FunctionsLogs.cs
@@ -29,44 +29,38 @@ namespace TranslationHelper.Functions
         /// <param name="WriteNow">If True then text will be now writed to log else only added to be writed later<br/></param>
         public void LogToFile(string TextToLog, bool WriteNow = true)
         {
-            if (THsbLog == null)
+            if (THsbLog == null) THsbLog = new StringBuilder();
+            if (!AppSettings.THdebug) return;
+            if (!WriteNow)
             {
-                THsbLog = new StringBuilder();
+                THsbLog.Append(DateTime.Now + " >>" + TextToLog + Environment.NewLine);
+                return;
             }
-            if (AppSettings.THdebug)
-            {
-                if (WriteNow)
-                {
-                    if (THsbLog.Length == 0)
-                    {
-                        FileWriter.WriteData(Path.Combine(Application.StartupPath, Application.ProductName + ".log")
-                            , Environment.NewLine
-                            + DateTime.Now + " >>" + TextToLog
-                            + Environment.NewLine
-                            + (DebugData.Count > 0 ? "Debug data:" + Environment.NewLine + string.Join(Environment.NewLine, DebugData) : "")
-                            , AppSettings.DebugMode);
-                    }
-                    else
-                    {
-                        FileWriter.WriteData(Path.Combine(Application.StartupPath, Application.ProductName + ".log")
-                            , Environment.NewLine
-                            + DateTime.Now + " >>" + THsbLog
-                            + Environment.NewLine
-                            + TextToLog
-                            + Environment.NewLine
-                            + (DebugData.Count > 0 ? "Debug data:" + Environment.NewLine + string.Join(Environment.NewLine, DebugData) : "")
-                            , AppSettings.DebugMode);
-                        //File.Move(Application.StartupPath + "\\TranslationHelper.log", Application.StartupPath + "\\TranslationHelper" + DateTime.Now.ToString("dd.MM.yyyy HH-mm-ss") + ".log");
-                        THsbLog.Clear();
-                    }
 
-                    DebugData.Clear();//clear debug data after they was logged
-                }
-                else
-                {
-                    THsbLog.Append(DateTime.Now + " >>" + TextToLog + Environment.NewLine);
-                }
+            if (THsbLog.Length == 0)
+            {
+                FileWriter.WriteData(Path.Combine(Application.StartupPath, Application.ProductName + ".log")
+                    , Environment.NewLine
+                    + DateTime.Now + " >>" + TextToLog
+                    + Environment.NewLine
+                    + (DebugData.Count > 0 ? "Debug data:" + Environment.NewLine + string.Join(Environment.NewLine, DebugData) : "")
+                    , AppSettings.DebugMode);
             }
+            else
+            {
+                FileWriter.WriteData(Path.Combine(Application.StartupPath, Application.ProductName + ".log")
+                    , Environment.NewLine
+                    + DateTime.Now + " >>" + THsbLog
+                    + Environment.NewLine
+                    + TextToLog
+                    + Environment.NewLine
+                    + (DebugData.Count > 0 ? "Debug data:" + Environment.NewLine + string.Join(Environment.NewLine, DebugData) : "")
+                    , AppSettings.DebugMode);
+                //File.Move(Application.StartupPath + "\\TranslationHelper.log", Application.StartupPath + "\\TranslationHelper" + DateTime.Now.ToString("dd.MM.yyyy HH-mm-ss") + ".log");
+                THsbLog.Clear();
+            }
+
+            DebugData.Clear();//clear debug data after they was logged
         }
     }
 }
