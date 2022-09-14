7916e302b9e67db692433d434f41c0609b86e818
TokcDK <TokcDK@gmail.com>
2022-09-14 20:25:46 +0300

    tweaks

--- TranslationHelper/Extensions/ExtensionsList.cs
+++ TranslationHelper/Extensions/ExtensionsList.cs
@@ -11,13 +11,7 @@ namespace TranslationHelper.Extensions
         /// <param name="hashset"></param>
         /// <param name="Key"></param>
         /// <param name="value"></param>
-        internal static void TryAdd<T>(this HashSet<T> hashset,T value)
-        {
-            if (!hashset.Contains(value))
-            {
-                hashset.Add(value);
-            }
-        }
+        internal static void TryAdd<T>(this HashSet<T> hashset, T value) { if (!hashset.Contains(value)) hashset.Add(value); }
 
         /// <summary>
         /// Add <paramref name="key"/>-<paramref name="value"/> if <paramref name="key"/> not exists in <paramref name="dictionary"/>
@@ -27,12 +21,7 @@ namespace TranslationHelper.Extensions
         /// <param name="dictionary"></param>
         /// <param name="key"></param>
         /// <param name="value"></param>
-        internal static void TryAdd<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value)
-        {
-            if (dictionary.ContainsKey(key)) return;
-
-            dictionary.Add(key, value);
-        }
+        internal static void TryAdd<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value) { if (!dictionary.ContainsKey(key)) dictionary.Add(key, value); }
 
         /// <summary>
         /// Remove <paramref name="key"/> if <paramref name="key"/> exists in <paramref name="dictionary"/>
@@ -41,12 +30,6 @@ namespace TranslationHelper.Extensions
         /// <typeparam name="TValue"></typeparam>
         /// <param name="dictionary"></param>
         /// <param name="key"></param>
-        internal static void TryRemove<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key)
-        {
-            if (dictionary.ContainsKey(key))
-            {
-                dictionary.Remove(key);
-            }
-        }
+        internal static void TryRemove<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key) { if (dictionary.ContainsKey(key)) dictionary.Remove(key); }
     }
 }
