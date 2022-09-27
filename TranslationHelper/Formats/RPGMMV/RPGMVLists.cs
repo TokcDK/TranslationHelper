a69aba7c6da9d408616c01d7745e560ecf45c802
TokcDK <TokcDK@gmail.com>
2022-09-27 18:28:35 +0500

    update rpgmv skip code list

--- TranslationHelper/Formats/RPGMMV/RPGMVLists.cs
+++ TranslationHelper/Formats/RPGMMV/RPGMVLists.cs
@@ -13,39 +13,39 @@ namespace TranslationHelper.Formats.RPGMMV
         /// </summary>
         internal static Dictionary<int, string> ExcludedCodes = new Dictionary<int, string>()
         {
-            { 41, "Image name?" }, // file name
-            { 44, "" }, // file name
-            { 45, "" }, // file name
-            //{ 118, "" }, // file name
-            //{ 122, "" }, // file name
-            { 123, "" }, // file name
-            { 132, "" }, // file name
-            { 231, "Show Picture" }, // file name
-            { 232, "Move Picture" }, // file name
-            { 233, "Rotate Picture" }, // file name
-            { 234, "Tint Picture" }, // file name
-            { 235, "Erase Picture" }, // file name
+            { 41, "Image name?" },
+            { 44, "" },
+            { 45, "" },
+            { 118, "label" },
+            //{ 122, "Control variables" },
+            { 123, "" },
+            { 132, "" },
+            { 231, "Show Picture" },
+            { 232, "Move Picture" },
+            { 233, "Rotate Picture" },
+            { 234, "Tint Picture" },
+            { 235, "Erase Picture" },
             { 236, "Set Weather" },
-            { 241, "Play BGM" }, // file name
-            { 242, "Fadeout BGM" }, // file name
-            { 243, "Save BGM" }, // file name
-            { 244, "Resume BGM" }, // file name
-            { 245, "Play BGS" }, // file name
-            { 246, "Fadeout BGS" }, // file name
-            { 249, "Play ME" }, // file name
-            { 250, "Play SE" }, // file name
-            { 251, "Stop SE" }, // file name
-            { 261, "Play Movie" }, // file name
-            { 283, "" }, // file name
-            { 320, "" }, // file name
-            { 322, "" }, // file name
-            { 323, "" }, // file name
-            //{ 355, "" }, // file name
-            { 356, "" }, // file name
-            { 357, "Plugin command" }, // file name
-            { 302, "" }, // file name
-            { 405, "" }, // file name
-            { 657, "Plugin command" }, // file name
+            { 241, "Play BGM" },
+            { 242, "Fadeout BGM" },
+            { 243, "Save BGM" },
+            { 244, "Resume BGM" },
+            { 245, "Play BGS" },
+            { 246, "Fadeout BGS" },
+            { 249, "Play ME" },
+            { 250, "Play SE" },
+            { 251, "Stop SE" },
+            { 261, "Play Movie" },
+            { 283, "" },
+            { 320, "" },
+            { 322, "" },
+            { 323, "" },
+            //{ 355, "Script command" }, // can contain text
+            { 356, "" },
+            { 357, "Plugin command" },
+            { 302, "" },
+            { 405, "" },
+            { 657, "Plugin command" },
             //{ 108, "Comment" }, // not to skip in some games
             //{ 408, "Comment" }, // not to skip in some games
         };
