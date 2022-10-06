98a6c885167e5ec78dedeeb871a3504f7f80c0f0
TokcDK <TokcDK@gmail.com>
2022-10-06 17:19:25 +0500

    add xua txt for mat for test

--- TH.WPF/Formats/FormatBase/IFormat.cs
+++ TH.WPF/Formats/FormatBase/IFormat.cs
@@ -8,15 +8,38 @@
         string Extension { get; }
 
         /// <summary>
+        /// Description of file for the format 
+        /// </summary>
+        string Description { get; }
+
+        /// <summary>
         /// Open actions to parse file with this parser
         /// </summary>
         /// <returns></returns>
-        bool Open();
+        bool Open(FileInfo Path);
 
         /// <summary>
         /// Save actions to parse file with this parser
         /// </summary>
         /// <returns></returns>
-        bool Save();
+        bool Save(FileInfo Path);
+
+        /// <summary>
+        /// List of strings
+        /// </summary>
+        List<StringData> StringsList { get; }
+    }
+
+    public class StringData
+    {
+        public StringData(string original, int index)
+        {
+            Original = original;
+            Index = index;
+        }
+
+        public int Index { get; set; }
+        public string Original { get; }
+        public string Translation { get; set; } = "";
     }
 }
