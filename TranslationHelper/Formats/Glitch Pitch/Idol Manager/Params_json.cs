ca184871a60cf71124567da05c0201e2cb1f1b19
TokcDK <TokcDK@gmail.com>
2022-10-17 12:16:20 +0500

    add params json opensave

---
+++ TranslationHelper/Formats/Glitch Pitch/Idol Manager/Params_json.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Newtonsoft.Json;
+
+namespace TranslationHelper.Formats.Glitch_Pitch.Idol_Manager
+{
+    internal class Params_json : FormatStringBase
+    {
+        public override string Ext => ".json";
+        protected override void ParseFileContent()
+        {
+            var infoContent = JsonConvert.DeserializeObject<Params_json_c>(ParseData.Reader.ReadToEnd());
+
+            bool ret = false;
+
+            string str = infoContent.FirstName;
+            if (IsParsed(ref str)) { ret = true; infoContent.FirstName = str; }
+            str = infoContent.LastName;
+            if (IsParsed(ref str)) { ret = true; infoContent.LastName = str; }
+            str = infoContent.IntroMessage;
+            if (IsParsed(ref str)) { ret = true; infoContent.IntroMessage = str; }
+
+            if (SaveFileMode && ret && ParseData.Ret)
+                ParseData.ResultForWrite.Append(JsonConvert.SerializeObject(infoContent));
+        }
+
+        private bool IsParsed(ref string stringToTranslate)
+        {
+            if (string.IsNullOrEmpty(stringToTranslate)) return false;
+
+            var ot = new[] { stringToTranslate, "" };
+            if (AddRowData(ref ot, rowInfo: nameof(stringToTranslate), isCheckInput: false))
+            {
+                if (SaveFileMode) stringToTranslate = ot[0]; // set to translated
+
+                return true;
+            };
+
+            return false;
+        }
+    }
+}
---
+++ TranslationHelper/Formats/Glitch Pitch/Idol Manager/Params_json_c.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TranslationHelper.Formats.Glitch_Pitch.Idol_Manager
+{
+    internal class Params_json_c
+    {
+        [JsonProperty("left")]
+        public long Left { get; set; }
+
+        [JsonProperty("top")]
+        public long Top { get; set; }
+
+        [JsonProperty("ignore_body")]
+        public long[] IgnoreBody { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("unique")]
+        public bool Unique { get; set; }
+
+        [JsonProperty("unique_id")]
+        public string UniqueId { get; set; }
+
+        [JsonProperty("age")]
+        public long Age { get; set; }
+
+        [JsonProperty("trait")]
+        public string Trait { get; set; }
+
+        [JsonProperty("intro_message")]
+        public string IntroMessage { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+
+        [JsonProperty("stats")]
+        public Stat[] Stats { get; set; }
+    }
+
+    public partial class Stat
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("val")]
+        public long Val { get; set; }
+
+        [JsonProperty("potential")]
+        public long Potential { get; set; }
+    }
+}
