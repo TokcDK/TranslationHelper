036bfe20dde5aff88784280a7991362b941243ee
TokcDK <TokcDK@gmail.com>
2022-11-06 17:47:54 +0500

    tweak

--- Current/Modules/THConfig/Interfaces/SettingsGroups/IToolsSettings.cs
+++ Current/Modules/THConfig/Interfaces/SettingsGroups/IToolsSettings.cs
@@ -12,7 +12,7 @@ namespace THConfig.Interfaces.SettingsGroups
         [DefaultValue(true)]
         bool UseOnlineTranslationCache { get; set; }
         [DefaultValue("https://translate.google.com/?ie=UTF-8&op=translate&sl={from}&tl={to}&text={text}")]
-        string WebTranslationLink { get; set; }
+        Uri WebTranslationLink { get; set; }
         [DefaultValue("G|https://translate.google.com/?ie=UTF-8&op=translate&sl={from}&tl={to}&text={text}||D|https://www.deepl.com/ru/translator#{from}/{to}/{text}||Y|https://translate.yandex.com/?lang={from}-{to}&text={text}")]
         string WebTranslationLinkServices { get; set; }
         [DefaultValue(true)]
