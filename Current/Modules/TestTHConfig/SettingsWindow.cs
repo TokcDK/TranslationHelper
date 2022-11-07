b29979abe13aa653453978daab7124812b578ad1
TokcDK <TokcDK@gmail.com>
2022-11-07 20:33:09 +0500

    add code to parse int and string values

--- Current/Modules/TestTHConfig/SettingsWindow.cs
+++ Current/Modules/TestTHConfig/SettingsWindow.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using System.ComponentModel;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using THConfig.Extensions;
 
 namespace TestTHConfig
 {
@@ -25,7 +19,14 @@ namespace TestTHConfig
             var rootFlowLayoutPanel = new FlowLayoutPanel();
             rootFlowLayoutPanel.Dock = DockStyle.Fill;
             rootFlowLayoutPanel.FlowDirection = FlowDirection.TopDown;
-            foreach(var p in THConfig.StaticSettings.Settings.GetType().GetProperties())
+
+            var settingsType = THConfig.StaticSettings.Settings.GetType();
+
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+
+            // https://stackoverflow.com/a/39336890 , get attributes for interface properties
+            var displayNameInfos = CustomAttributeExtractorExtensions.GetPropertyAttributesFromType<DisplayNameAttribute>(settingsType);
+            foreach (var p in THConfig.StaticSettings.Settings.GetType().GetProperties())
             {
                 var t = p.GetMethod.ReturnType;
                 if (t == typeof(bool))
@@ -33,17 +34,37 @@ namespace TestTHConfig
                     var cb = new CheckBox();
                     cb.AutoSize = true;
 
-                    var attributes = p.GetCustomAttributes(true);
-
                     // set parameter display name
-                    var displayName = attributes.FirstOrDefault(a => (a as DisplayNameAttribute) != null);
-                    cb.Text = displayName != null ? (displayName as DisplayNameAttribute).DisplayName : p.Name;
+                    var displayNameInfo = displayNameInfos.FirstOrDefault(i => i.Property.Name == p.Name && i.Property.GetMethod.ReturnType == t);
+                    cb.Text = displayNameInfo != null ? displayNameInfo.Attribute.DisplayName : p.Name;
 
                     // binding value
                     cb.DataBindings.Add(new Binding("Checked", THConfig.StaticSettings.Settings, p.Name));
                     rootFlowLayoutPanel.Controls.Add(cb);
                 }
-                break;
+                else if (t == typeof(int) || t == typeof(string))
+                {
+                    var flp = new FlowLayoutPanel();
+                    flp.AutoSize = true;
+
+                    var lbl = new Label();
+                    lbl.AutoSize = true;
+                    var tbx = new TextBox();
+                    tbx.AutoSize = true;
+
+                    // set parameter display name
+                    var displayNameInfo = displayNameInfos.FirstOrDefault(i => i.Property.Name == p.Name && i.Property.GetMethod.ReturnType == t);
+                    lbl.Text = displayNameInfo != null ? displayNameInfo.Attribute.DisplayName : p.Name;
+
+                    // binding value
+                    tbx.DataBindings.Add(new Binding("Text", THConfig.StaticSettings.Settings, p.Name));
+
+                    flp.Controls.Add(lbl);
+                    flp.Controls.Add(tbx);
+                    flp.Dock = DockStyle.Fill;
+
+                    rootFlowLayoutPanel.Controls.Add(flp);
+                }
             }
 
             rootPanel.Controls.Add(rootFlowLayoutPanel);
