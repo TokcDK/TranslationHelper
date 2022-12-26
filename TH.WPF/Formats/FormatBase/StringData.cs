b0a9902c815539ff3c0797a993a55038cd242156
TokcDK <TokcDK@gmail.com>
2022-12-26 22:46:29 +0500

    edit string data, removed index

--- TH.WPF/Formats/FormatBase/StringData.cs
+++ TH.WPF/Formats/FormatBase/StringData.cs
@@ -2,13 +2,11 @@
 {
     public class StringData
     {
-        public StringData(string original, int index)
+        public StringData(string original)
         {
             Original = original;
-            Index = index;
         }
 
-        public int Index { get; set; }
         public string Original { get; }
         public string Translation { get; set; } = "";
     }
