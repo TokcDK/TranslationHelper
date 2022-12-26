e9ead4f3c7f59b1909612c794804e30d833f7a70
TokcDK <TokcDK@gmail.com>
2022-12-26 15:43:05 +0500

    add rpgmv stopcv txt scenarion format

---
+++ Current/Main/Formats/RPGMMV/StopCVPluginTXT.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslationHelper.Formats.RPGMMV
+{
+    internal class StopCVPluginTXT : FormatStringBase
+    {
+        public override string Extension => ".txt";
+
+        protected override KeywordActionAfter ParseStringFileLine()
+        {
+            if (!IsValid(ParseData.Line))
+            {
+                SaveModeAddLine();
+                return KeywordActionAfter.Continue; // comment
+            }
+
+            var t = string.Join("\r\n", GetLines());
+            AddRowData(ref t);
+
+            SaveModeAddLine();
+
+            return base.ParseStringFileLine();
+        }
+
+        private IEnumerable<string> GetLines()
+        {
+            while (IsValid(ReadLine())) 
+                yield return ParseData.Line;
+        }
+
+        bool IsValid(string s)
+        {
+            if (ParseData.Line.StartsWith("#")) return false; // comment
+            if (ParseData.Line.StartsWith("@")) return false; // command
+            if (string.IsNullOrEmpty(ParseData.Line)) return false; // empty
+
+            return true;
+        }
+    }
+}
