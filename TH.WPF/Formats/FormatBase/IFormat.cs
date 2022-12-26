f23e854f4dda883bfeca51865c5037c8ff3275d7
TokcDK <TokcDK@gmail.com>
2022-12-26 22:32:51 +0500

    ..

--- TH.WPF/Formats/FormatBase/IFormat.cs
+++ TH.WPF/Formats/FormatBase/IFormat.cs
@@ -29,17 +29,4 @@
         /// </summary>
         List<StringData> StringsList { get; }
     }
-
-    public class StringData
-    {
-        public StringData(string original, int index)
-        {
-            Original = original;
-            Index = index;
-        }
-
-        public int Index { get; set; }
-        public string Original { get; }
-        public string Translation { get; set; } = "";
-    }
 }
---
+++ TH.WPF/Formats/FormatBase/StringData.cs
@@ -0,0 +1,15 @@
+﻿namespace FormatBase
+{
+    public class StringData
+    {
+        public StringData(string original, int index)
+        {
+            Original = original;
+            Index = index;
+        }
+
+        public int Index { get; set; }
+        public string Original { get; }
+        public string Translation { get; set; } = "";
+    }
+}
