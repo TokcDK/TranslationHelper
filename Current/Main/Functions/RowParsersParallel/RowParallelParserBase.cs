02a5ab0e6ba6c9861139c83fb26aae83f33cad05
TokcDK <TokcDK@gmail.com>
2023-05-12 11:40:44 +0500

    add some wip code of parallel rows parser

---
+++ Current/Main/Functions/RowParsersParallel/RowParallelParserBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslationHelper.Functions.RowParsersParallel
+{
+    public abstract class RowParallelParserBase
+    {
+        #region Tables
+        void ParseSelectedTables(DataTable[] tables)
+        {
+            _rowsLeftToProcess = tables.Select(t => t.Rows.Count).Sum();
+
+            Parallel.ForEach(tables, table => 
+            {
+                if (!IsValidTable(table)) return;
+
+                Parse(table); 
+            });
+        }
+
+        private bool IsValidTable(DataTable table)
+        {
+            return true;
+        }
+
+        void Parse(DataTable table)
+        {
+            ParseSelectedRows(table.Rows.AsParallel().OfType<DataRow>());
+        }
+
+        #endregion Tables
+
+        #region Rows
+
+        /// <summary>
+        /// Determine if the row is last processing
+        /// </summary>
+        protected bool IsLastRow = false;
+        int _rowsLeftToProcess = 0;
+
+        void ParseSelectedRows(IEnumerable<DataRow> rows)
+        {
+            Parallel.ForEach(rows, row => 
+            {
+                IsLastRow = --_rowsLeftToProcess == 0;
+
+                if (!IsValidRow(row)) return;
+
+                Parse(row); 
+            });
+        }
+
+        protected virtual bool IsValidRow(DataRow row)
+        {
+            return true;
+        }
+
+        protected abstract bool Parse(DataRow row);
+
+        #endregion Rows
+    }
+}
