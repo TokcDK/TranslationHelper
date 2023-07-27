4045e4a4f919b472c4886a1b0cff79adc92fac39
TokcDK <TokcDK@gmail.com>
2023-07-27 19:39:35 +0500

    add xlsx db format

---
+++ Current/Main/Functions/DBSaveFormats/Xlsx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using MiniExcelLibs;
+
+namespace TranslationHelper.Functions.DBSaveFormats
+{
+    internal class Xlsx : IDataBaseFileFormat
+    {
+        public string Ext => "xlsx";
+
+        public string Description => $"({Ext}) Excel xlsx format";
+
+        public void Read(string fileName, object data)
+        {
+            var sheetNames = MiniExcel.GetSheetNames(fileName);
+            foreach (var sheetName in sheetNames)
+            {
+                foreach(var row in MiniExcel.Query(fileName, sheetName: sheetName))
+                {
+                }
+            }
+        }
+
+        public void Write(string fileName, object data)
+        {
+            MiniExcel.SaveAs(fileName, data);
+        }
+    }
+}
