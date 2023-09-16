2ad07f342502d0c423220d3e4f16a6b06793b885
TokcDK <TokcDK@gmail.com>
2023-09-16 11:05:42 +0500

    add function to get matching translation regex for selecterd rows

---
+++ Current/Main/Functions/FileElementsFunctions/Row/ShowTranslationRegex.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Shapes;
+
+namespace TranslationHelper.Functions.FileElementsFunctions.Row
+{
+    internal class ShowTranslationRegex : RowBase
+    {
+        readonly List<ExtractRegexInfo> _found = new List<ExtractRegexInfo>();
+        int addedCount = 0;
+        protected override bool Apply(RowBaseRowData rowData)
+        {
+            if (addedCount > 10) return false;
+
+            var extractData = new ExtractRegexInfo(rowData.Original);
+            if (extractData == null)
+            {
+                return false;
+            }
+
+            _found.Add(extractData);
+            addedCount++;
+
+            return true;
+        }
+
+        protected override void ActionsFinalize()
+        {
+            if(_found.Count > 0 )
+            {
+                var lineString = T._("Line");
+                var patternString = T._("Pattern");
+                var replacerString = T._("Replacer");
+
+                string resultMessage = string.Join("\r\n\r\n",
+                    _found.Select(el =>
+                    $"{lineString}:" + el.InputString +
+                    $"\n{patternString}:" + el.Pattern +
+                    $"\n: {replacerString}:" + el.Replacer));
+
+                Clipboard.Clear();
+                Clipboard.SetText(resultMessage);
+
+                MessageBox.Show(resultMessage + $"\r\n\r\n({T._("Copied to clipboard")})");
+            }
+            else { MessageBox.Show(T._("No matching translation regex found.."));  }
+        }
+    }
+}
---
+++ Current/Main/Menus/FileRowMenus/ShowTranslationRegexMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TranslationHelper.Functions.FileElementsFunctions.Row;
+using TranslationHelper.Menus.FilesListMenus;
+
+namespace TranslationHelper.Menus.FileRowMenus
+{
+    internal class ShowTranslationRegexMenu : FileRowMenuItemBase
+    {
+        public override string Text => T._("Show matching translation regex");
+
+        public override void OnClick(object sender, EventArgs e)
+        {
+            new ShowTranslationRegex().Rows();
+        }
+    }
+}
