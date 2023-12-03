33eb73642f0e119fa2f9501fe5565589532e30b5
TokcDK <TokcDK@gmail.com>
2023-12-03 20:42:49 +0500

    add ai6game project

---
+++ Current/Main/Projects/AI6Win/AI6WinGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TranslationHelper.Data;
+using TranslationHelper.Formats.AI6WIN;
+
+namespace TranslationHelper.Projects.AI6Win
+{
+    internal class AI6WinGame : ProjectBase
+    {
+        public override string Name => "AI6Win Game";
+
+        public override bool Open()
+        {
+            return OpenSave();
+        }
+
+        public override bool Save()
+        {
+            return OpenSave();
+        }
+        bool OpenSave()
+        {
+            return ProjectToolsOpenSave.OpenSaveFilesBase(this, Path.GetDirectoryName(AppData.SelectedProjectFilePath), typeof(AI6Arc), "mes.arc");
+        }
+
+        internal override bool IsValid()
+        {
+            if (!ProjectTools.IsExe(AppData.SelectedProjectFilePath)) return false;
+            if (!string.Equals(Path.GetFileName(AppData.SelectedProjectFilePath)
+                , "AI6WIN.exe", StringComparison.InvariantCultureIgnoreCase)) return false;
+
+            var dir = Path.GetDirectoryName(AppData.SelectedProjectFilePath);
+
+            return File.Exists(Path.Combine(dir, "mes.arc"));
+        }
+    }
+}
