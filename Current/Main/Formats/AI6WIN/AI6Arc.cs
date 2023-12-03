dd9176a7411966e0d307e406c75066f81a2dfcba
TokcDK <TokcDK@gmail.com>
2023-12-03 12:10:07 +0500

    add ai6 arc format add arc parsers

---
+++ Current/Main/Formats/AI6WIN/AI6Arc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslationHelper.Formats.AI6WIN
+{
+    internal class AI6Arc : FormatBinaryBase
+    {
+        public override string Extension => ".arc";
+
+        protected override void FileOpen()
+        {
+
+        }
+    }
+}
