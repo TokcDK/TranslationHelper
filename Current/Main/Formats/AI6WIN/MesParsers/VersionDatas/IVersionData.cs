e5da34c7008d72eb7088327b0839f1134ed08397
TokcDK <TokcDK@gmail.com>
2023-12-03 22:07:33 +0500

    removed old ai6 script parser

--- Current/Main/Formats/AI6WIN/MesParsers/VersionDatas/IVersionData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MesScriptDissAss
-{
-    internal interface IVersionData
-    {
-        string Name { get; }
-        (byte Opcode, string Struct, string Name)[] CommandLibrary { get; }
-    }
-
-}
