ce1d081d19c6dd8567a39de30e4f96f8cd930fb6
TokcDK <TokcDK@gmail.com>
2024-03-08 20:22:19 +0500

    add main required properties and infos

--- TH.WPF/Formats/FormatBase/IFormatParser.cs
+++ TH.WPF/Formats/FormatBase/IFormatParser.cs
@@ -8,6 +8,12 @@ namespace FormatBase
 {
     public class FileExtractedStringInfo
     {
+        /// <summary>
+        /// Data of extracted string by the <seealso cref="IFormatParser"/>
+        /// </summary>
+        /// <param name="originalText">Text of original</param>
+        /// <param name="translationText">Optional  text of translation</param>
+        /// <param name="infoText">Optional text of useful information of the string</param>
         public FileExtractedStringInfo(string originalText, string? translationText, string? infoText)
         {
             OriginalText = originalText;
@@ -15,13 +21,34 @@ namespace FormatBase
             InfoText = infoText;
         }
 
+        /// <summary>
+        /// String original text
+        /// </summary>
         public string OriginalText { get; }
+
+        /// <summary>
+        /// Text of the original translation
+        /// </summary>
         public string? TranslationText { get; set; }
+
+        /// <summary>
+        /// Info text for the extracted string
+        /// </summary>
         public string? InfoText { get; set; }
     }
 
     public interface IFormatParser
     {
+        /// <summary>
+        /// Valid file extensions which can be parsed by the format
+        /// </summary>
+        string[] ValidExtensions { get; }
+
+        /// <summary>
+        /// Short description of te file format
+        /// </summary>
+        string ShortDescription { get; }
+
         /// <summary>
         /// Extract strings from <paramref name="fileContent"/>
         /// </summary>
@@ -29,6 +56,11 @@ namespace FormatBase
         /// <returns></returns>
         List<FileExtractedStringInfo> ExtractStrings(Stream fileContent);
 
+        /// <summary>
+        /// Write strings back into file
+        /// </summary>
+        /// <param name="fileExtractedStringInfos"></param>
+        /// <returns></returns>
         Stream WriteStrings(List<FileExtractedStringInfo> fileExtractedStringInfos);
     }
 }
