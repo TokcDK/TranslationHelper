36607afeb1dae053119756bbc1694e458d857443
TokcDK <TokcDK@gmail.com>
2025-07-07 11:45:33 +0500

    fix save param

--- Current/Main/Functions/FunctionsSave.cs
+++ Current/Main/Functions/FunctionsSave.cs
@@ -40,7 +40,7 @@ namespace TranslationHelper.Functions
             if (AppData.CurrentProject != null)
             {
                 AppData.CurrentProject.BakCreate();
-                await Task.Run(() => AppData.CurrentProject.SaveDDDD(int fileIndexToSave = -1)).ConfigureAwait(true);
+                await Task.Run(() => AppData.CurrentProject.Save()).ConfigureAwait(true);
             }
             else
             {
