0023754c9e19f287cb7fa6628c7c0c8124e98208
TokcDK <TokcDK@gmail.com>
2025-11-12 13:56:42 +0500

    add form with buttons

---
+++ Current/Main/Forms/Search/Search.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace TranslationHelper.Forms.Search
+{
+    partial class Search
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchRootPanel = new System.Windows.Forms.Panel();
+            this.SearchRootTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.SearchButtonsTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SearchRootPanel.SuspendLayout();
+            this.SearchRootTableLayoutPanel.SuspendLayout();
+            this.SearchButtonsTableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SearchRootPanel
+            // 
+            this.SearchRootPanel.Controls.Add(this.SearchRootTableLayoutPanel);
+            this.SearchRootPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SearchRootPanel.Location = new System.Drawing.Point(0, 0);
+            this.SearchRootPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.SearchRootPanel.Name = "SearchRootPanel";
+            this.SearchRootPanel.Size = new System.Drawing.Size(765, 418);
+            this.SearchRootPanel.TabIndex = 0;
+            // 
+            // SearchRootTableLayoutPanel
+            // 
+            this.SearchRootTableLayoutPanel.ColumnCount = 2;
+            this.SearchRootTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65.5F));
+            this.SearchRootTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34.5F));
+            this.SearchRootTableLayoutPanel.Controls.Add(this.SearchButtonsTableLayoutPanel, 1, 0);
+            this.SearchRootTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SearchRootTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.SearchRootTableLayoutPanel.Margin = new System.Windows.Forms.Padding(1);
+            this.SearchRootTableLayoutPanel.Name = "SearchRootTableLayoutPanel";
+            this.SearchRootTableLayoutPanel.RowCount = 2;
+            this.SearchRootTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 74F));
+            this.SearchRootTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26F));
+            this.SearchRootTableLayoutPanel.Size = new System.Drawing.Size(765, 418);
+            this.SearchRootTableLayoutPanel.TabIndex = 0;
+            // 
+            // SearchButtonsTableLayoutPanel
+            // 
+            this.SearchButtonsTableLayoutPanel.ColumnCount = 1;
+            this.SearchButtonsTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.SearchButtonsTableLayoutPanel.Controls.Add(this.button4, 0, 3);
+            this.SearchButtonsTableLayoutPanel.Controls.Add(this.button3, 0, 2);
+            this.SearchButtonsTableLayoutPanel.Controls.Add(this.button2, 0, 1);
+            this.SearchButtonsTableLayoutPanel.Controls.Add(this.button1, 0, 0);
+            this.SearchButtonsTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SearchButtonsTableLayoutPanel.Location = new System.Drawing.Point(511, 10);
+            this.SearchButtonsTableLayoutPanel.Margin = new System.Windows.Forms.Padding(10);
+            this.SearchButtonsTableLayoutPanel.Name = "SearchButtonsTableLayoutPanel";
+            this.SearchButtonsTableLayoutPanel.RowCount = 4;
+            this.SearchButtonsTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.SearchButtonsTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.SearchButtonsTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.SearchButtonsTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.SearchButtonsTableLayoutPanel.Size = new System.Drawing.Size(244, 289);
+            this.SearchButtonsTableLayoutPanel.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(3, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(238, 66);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(3, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(238, 66);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Location = new System.Drawing.Point(3, 219);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(238, 67);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Search
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 418);
+            this.Controls.Add(this.SearchRootPanel);
+            this.Name = "Search";
+            this.Text = "Search";
+            this.SearchRootPanel.ResumeLayout(false);
+            this.SearchRootTableLayoutPanel.ResumeLayout(false);
+            this.SearchButtonsTableLayoutPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel SearchRootPanel;
+        private System.Windows.Forms.TableLayoutPanel SearchRootTableLayoutPanel;
+        private System.Windows.Forms.TableLayoutPanel SearchButtonsTableLayoutPanel;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
