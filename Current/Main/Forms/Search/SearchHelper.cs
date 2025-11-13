073f574fafff1017d67aebb55a4d84e1ae600f0b
TokcDK <TokcDK@gmail.com>
2025-11-13 16:11:39 +0500

    add classes containing search data

--- Current/Main/Forms/Search/SearchHelper.cs
+++ Current/Main/Forms/Search/SearchHelper.cs
@@ -8,8 +8,34 @@ namespace TranslationHelper.Forms.Search
 {
     internal class SearchHelper
     {
-        internal void SearchAll(SearchForm searchForm) 
+        internal List<SearchCondition> SearchConditions { get; set; } = new List<SearchCondition>();
+
+        internal SearchHelper()
         {
         }
+    }
+
+    internal class SearchCondition
+    {
+        internal string FindWhat { get; set; } = "";
+
+        internal SearchOptions Options { get; set; } = new SearchOptions();
+
+        internal List<Replacer> Replacers { get; set; } = new List<Replacer>();
+    }
+
+    internal class SearchOptions
+    {
+        internal List<string> SearchColumn { get; set; }
+
+        internal bool IsCaseSensitive { get; set; } = false;
+
+        internal bool IsRegex { get; set; } = false;
+    }
+
+    internal class Replacer
+    {
+        internal string ReplaceWhat { get; set; }
+        internal string ReplaceWith { get; set; }
     }
 }
