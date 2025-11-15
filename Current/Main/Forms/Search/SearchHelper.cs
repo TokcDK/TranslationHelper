83f1fade901c677082b1a9218837304d40af3c1e
TokcDK <TokcDK@gmail.com>
2025-11-15 18:40:33 +0500

    add test search option instances

--- Current/Main/Forms/Search/SearchHelper.cs
+++ Current/Main/Forms/Search/SearchHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace TranslationHelper.Forms.Search
 {
@@ -20,6 +21,50 @@ namespace TranslationHelper.Forms.Search
     {
         string ReplaceWhat { get; }
         string ReplaceWith { get; }
+    }
+    public interface ISearchOption
+    {
+        bool IsMatch(string inputString, string pattern);
+        string Replace(string inputString, string replaceWhat, string replaceWith);
+    }
+    public interface ISearchOptionUsingControl
+    {
+        Control Control { get; }
+    }
+
+    public class SearchOptionCaseSensitive : ISearchOption, ISearchOptionUsingControl
+    {
+        readonly CheckBox _control = new CheckBox() { Checked = false };
+
+        public Control Control => _control;
+
+        protected bool CaseSensitive => _control.Checked;
+
+        public bool IsMatch(string inputString, string pattern)
+        {
+            var comparison = CaseSensitive ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase;
+            return inputString.IndexOf(pattern, comparison) != -1;
+        }
+
+        public string Replace(string inputString, string replaceWhat, string replaceWith)
+        {
+            var options = CaseSensitive ? StringComparison.InvariantCulture : StringComparison.InvariantCultureIgnoreCase;
+            return inputString.Replace(replaceWhat, replaceWith, options);
+        }
+    }
+    public class SearchOptionRegex : SearchOptionCaseSensitive
+    {
+        public new bool IsMatch(string inputString, string pattern)
+        {
+            var options = CaseSensitive ? RegexOptions.None : RegexOptions.IgnoreCase;
+            return Regex.IsMatch(inputString, pattern, options);
+        }
+
+        public new string Replace(string inputString, string replaceWhat, string replaceWith)
+        {
+            var options = CaseSensitive ? RegexOptions.None : RegexOptions.IgnoreCase;
+            return Regex.Replace(inputString, replaceWhat, replaceWith, options);
+        }
     }
 
     public static class SearchHelpers
@@ -51,7 +96,7 @@ namespace TranslationHelper.Forms.Search
                 if (useRegex)
                 {
                     var options = caseSensitive ? RegexOptions.None : RegexOptions.IgnoreCase;
-                    result = Regex.Replace(result, task.ReplaceWhat, task.ReplaceWith ?? string.Empty, options);
+                    result = Regex.Replace(result, task.ReplaceWhat, task.ReplaceWith, options);
                 }
                 else
                 {
